d0e4eb96d31a0f299de913e8fa7a0db80b06479e
sydneysteele03 <126841347+sydneysteele03@users.noreply.github.com>
2023-06-13 21:43:46 -0700

    finished sql files

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -1,37 +1,51 @@
+--CREATE TABLES AND IMPORT DATA FROM CSV FILES 
+--contacts, campaign, categories and subcategories
+
 CREATE TABLE contacts (
-	contact_id INT, 
-	email varchar,
-	first_name varchar,
-	last_name varchar,
+	contact_id INT NOT NULL, 
+	email VARCHAR,
+	first_name VARCHAR,
+	last_name VARCHAR,
 	PRIMARY KEY(contact_id)
 );
 
 CREATE TABLE categories (
-	category_id VARCHAR,
+	category_id VARCHAR NOT NULL,
 	category VARCHAR,
 	PRIMARY KEY (category_id)
 ) ;
 
 CREATE TABLE subcategories (
-	subcategory_id varchar,
-	subcategory varchar,
+	subcategory_id VARCHAR NOT NULL,
+	subcategory VARCHAR,
 	PRIMARY KEY(subcategory_id)
 );
 
 CREATE TABLE campaign (
-	cf_id INT ,
+	cf_id INT NOT NULL,
 	contact_id INT references contacts(contact_id),
-	company_name varchar,
-	description varchar,
-	goal decimal,
-	pledged decimal, 
-	outcome varchar, 
+	company_name VARCHAR,
+	description VARCHAR,
+	goal REAL,
+	pledged REAL, 
+	outcome VARCHAR, 
 	backers_count INT,
-	country char,
-	currency char,
-	launched_date timestamp,
-	end_date timestamp,
-	category_id varchar references categories(category_id),
-	subcategory_id varchar references subcategories(subcategory_id),
+	country VARCHAR,
+	currency VARCHAR,
+	launched_date DATE,
+	end_date DATE,
+	category_id VARCHAR references categories(category_id),
+	subcategory_id VARCHAR references subcategories(subcategory_id),
 	PRIMARY KEY(cf_id)
 );
+
+select * from campaign;
+
+
+
+
+
+
+
+
+
